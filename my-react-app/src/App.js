fe83d2b939c1f96e72e455bd3e2b2872af92e08b
Hae-gun <newchk610@gmail.com>
2024-07-09 00:06:17 +0900

    refactor : source rollback

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -9,7 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button>hello</button>
         <a
           className="App-link"
           href="https://reactjs.org"
